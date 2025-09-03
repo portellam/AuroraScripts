02999eda862eef56f0e97f432d6393f8a87cfb08
portellam <amportell.13@gmail.com>
2025-09-03 12:46:21 -0400

    Refactored further.

--- Filters/local_backups.lua
+++ Filters/local_backups.lua
@@ -1,19 +1,11 @@
 GameListFilterCategories.User["Show Local Backups"] = function(Content)
-
-	local isNXE2GODBackup = (string.match(Content.Directory, "0000000000000000\\[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\00007000\\backup") ~= nil)
-	local isBAKFile = (string.lower(string.sub(Content.Executable, -4)) == ".bak")
-
-	return isNXE2GODBackup or isBAKFile
+    local isNXE2GODBackup = (string.match(Content.Directory, "0000000000000000\\[a-fA-F0-9]{8}\\00007000\\backup") ~= nil)
+    local isBAKFile = (string.lower(string.sub(Content.Executable, -4)) == ".bak")
+    return isNXE2GODBackup or isBAKFile
 end
 
 GameListFilterCategories.User["Hide Local Backups"] = function(Content)
-
-	local isNXE2GODBackup = (string.match(Content.Directory, "0000000000000000\\[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\00007000\\backup") ~= nil)
-	local isBAKFile = (string.lower(string.sub(Content.Executable, -4)) == ".bak")
-
-	if isNXE2GODBackup then
-		return false
-  end
-
-	return not isBAKFile
+    local isNXE2GODBackup = (string.match(Content.Directory, "0000000000000000\\[a-fA-F0-9]{8}\\00007000\\backup") ~= nil)
+    local isBAKFile = (string.lower(string.sub(Content.Executable, -4)) == ".bak")
+    return not isNXE2GODBackup and not isBAKFile
 end
