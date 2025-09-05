00fffc02692e23d5cda083c02ed35c0f9b38f7b2
portellam <amportell.13@gmail.com>
2025-09-04 23:37:15 -0400

    Rename.

--- Filters/local_multiplayer.lua
+++ Filters/local_multiplayer.lua
@@ -1,13 +1,13 @@
-local function is_local_coop(Content)
+local function is_match(Content)
 	local minco = bit32.rshift(bit32.band(Content.CapabilitiesOffline.LowPart, 0x00FF0000), 16)
 	local maxco = bit32.rshift(bit32.band(Content.CapabilitiesOffline.LowPart, 0xFF000000), 24)
 	return (minco >= 1) and (maxco >= 2)
 end
 
 GameListFilterCategories.User["Show Local Multiplayer"] = function(Content)
-	return is_local_coop(Content)
+	return is_match(Content)
 end
 
 GameListFilterCategories.User["Hide Local Multiplayer"] = function(Content)
-	return not is_local_coop(Content)
+	return not is_match(Content)
 end
